731ba6a16e983a5e3025876b4852c6a1cd389273
HunterJoe <hunterjoe9999@gmail.com>
2024-12-20 20:12:04 +0900

    데이터 변경했음

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import styled from 'styled-components';
-import Section from './Section';
-import sectionData from './section.json';
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
+import sectionData from "./section.json";
 
 function Home() {
-  return (
-    //TODO: map으로 돌려서 좀 깔끔하게 만들자
-    <Container>
-      {sectionData.map((info,i) => {
-        return (
-          <Section 
-            key={i}
-            title={info.title}
-            description={info.description}
-            backgroundImg={info.backgroundImg}
-            leftBtnText={info.leftBtnText}
-            rightBtnText={info.rightBtnText}
-          />
-        );
-      })}
-    </Container>
-  )
+	return (
+		// something chnag
+		<Container>
+			{sectionData.map((info, i) => {
+				return (
+					<Section
+						key={i}
+						title={info.title}
+						description={info.description}
+						backgroundImg={info.backgroundImg}
+						leftBtnText={info.leftBtnText}
+						rightBtnText={info.rightBtnText}
+					/>
+				);
+			})}
+		</Container>
+	);
 }
 
-export default Home
+export default Home;
 
 const Container = styled.div`
-  height: 100vh;
+	height: 100vh;
 `;
